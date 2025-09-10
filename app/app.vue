1648cc63009f8b84ae51c14515d1b9d8ead43216
xlsama <im.xlsama@gmail.com>
2025-09-10 16:49:53 +0800

    chore: update

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { zh_cn } from '@nuxt/ui/locale'
+import { motion } from 'motion-v'
 
 const open = (url: string) => {
   window.open(url, '_blank')
@@ -8,32 +9,40 @@ const open = (url: string) => {
 
 <template>
   <UApp :locale="zh_cn">
-    <UContainer class="mt-30">
-      <h1 class="text-4xl font-semibold">
-        李小龙
-      </h1>
-      <div class="mt-10 space-y-10">
-        <div class="flex items-center space-x-4">
-          <UIcon
-            name="i-lucide-github"
-            class="
-              size-5 transition-all
-              hover:scale-110
-              active:scale-95
-            "
-            @click="open('https://github.com/xlsama')"
-          />
-          <UIcon
-            name="i-custom-x"
-            class="
-              size-4.5 transition-all
-              hover:scale-110
-              active:scale-95
-            "
-            @click="open('https://x.com/xlsama_')"
-          />
+    <motion.div
+      :animate="{ opacity: 1, y: 0 }"
+      :initial="{ opacity: 0, y: 10 }"
+      :transition="{ ease: 'easeInOut' }"
+    >
+      <UContainer class="mt-30">
+        <h1 class="text-4xl font-semibold">
+          李小龙
+        </h1>
+        <div class="mt-10 space-y-10">
+          <div class="flex items-center space-x-4">
+            <motion.div
+              :while-hover="{ scale: 1.1 }"
+              :while-press="{ scale: 0.95 }"
+            >
+              <UIcon
+                name="i-lucide-github"
+                class="size-5"
+                @click="open('https://github.com/xlsama')"
+              />
+            </motion.div>
+            <motion.div
+              :while-hover="{ scale: 1.1 }"
+              :while-press="{ scale: 0.95 }"
+            >
+              <UIcon
+                name="i-custom-x"
+                class="size-4.5"
+                @click="open('https://x.com/xlsama_')"
+              />
+            </motion.div>
+          </div>
         </div>
-      </div>
-    </UContainer>
+      </UContainer>
+    </motion.div>
   </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
     '@vueuse/nuxt',
+    'motion-v/nuxt',
   ],
   imports: {
     presets: [{ from: 'dayjs', imports: [{ name: 'default', as: 'dayjs' }] }],
